ea02b5fc5f6a3d02c0b6a9133f4f1eabf6cea621
Yogan-bharath <yoganbharath@gmail.com>
2026-09-10 22:34:39 +0530

    added backend link

--- src/features/auth/services/auth.api.js
+++ src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { TurkishLira } from "lucide-react";
 
 const authAPI = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://hire-prep-ai-back-end.vercel.app",
     withCredentials:true
 })
 
--- src/features/interview/services/interview.api.js
+++ src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios  from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://hire-prep-ai-back-end.vercel.app",
     withCredentials:true
 })
 
